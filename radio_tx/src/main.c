5de133a4c132023a68e7af8f5c68b5c3bb20258d
Andrew Goessling <agoessling@gmail.com>
2022-11-20 15:51:59 -0800

    Polling message queues.

--- radio_tx/src/main.c
+++ radio_tx/src/main.c
@@ -4,75 +4,97 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/sys/notify.h>
+#include <zephyr/sys/ring_buffer.h>
 
-#define TRANSMIT_PACKET_LEN 248
-#define RX_TIMEOUT_US 500
+// Common defines.
+#define NUM_PIPES 2
 
+// TX radio defines
+#define TX_MAX_PKT_LEN 248
+#define TX_RETRANSMIT_DELAY_US 600
+#define TX_RETRANSMIT_COUNT 3
+
+// UART defines
 #define UART uart0
-#define BAUD 921600
-#define NUM_UART_RX_BUF 4
-
-#define RETRANSMIT_DELAY_US 600
-#define RETRANSMIT_COUNT 3
+#define UART_BAUD 921600
+#define UART_TIMEOUT_US 50
+#define UART_NUM_SLABS 8
+#define UART_EVENT_QUEUE_LEN 20
 
 LOG_MODULE_REGISTER(radio_tx, CONFIG_RADIO_TX_APP_LOG_LEVEL);
 
 typedef struct {
-  uint8_t buf[NUM_UART_RX_BUF][TRANSMIT_PACKET_LEN];
-  uint32_t write_index;  // Index of buffer currently being written by uart.
-  uint8_t *read_buf;  // Pointer to buffer currently being read by main thread.
-} UartRxSlab;
-
-typedef struct {
-  struct uart_event_rx event;
-  uint8_t pipe;
-} UartRxPacket;
-
-typedef struct {
-  uint32_t rx_overflow;
-  uint32_t pkt_overflow;
-  uint32_t other;
+  atomic_t rx_overflow;
+  atomic_t pkt_overflow;
+  atomic_t poll_fail;
+  atomic_t data_not_available;
+  atomic_t queue_fail;
+  atomic_t other;
 } ErrorCount;
 
 typedef struct {
   ErrorCount errors;
-  uint32_t rx_bytes;
+  atomic_t rx_bytes;
 } Stats;
-
-static UartRxSlab g_uart_rx_slab = {.write_index = 0, .read_buf = (uint8_t *)g_uart_rx_slab.buf};
-
-K_MSGQ_DEFINE(g_rx_packet_queue, sizeof(UartRxPacket), 20, 1);
 
 static Stats g_stats;
 
+typedef struct {
+  uint8_t buf[UART_NUM_SLABS][TX_MAX_PKT_LEN];
+  uint32_t write_slab_index;  // Index of slab currently being written by uart.
+  atomic_ptr_t read_slab;  // Pointer to slab currently being read by main thread.
+} UartRxSlabs;
+
+static UartRxSlabs g_uart_rx_slabs[NUM_PIPES] = {
+    {.write_slab_index = 0, .read_slab = ATOMIC_PTR_INIT(g_uart_rx_slabs[0].buf)},
+    {.write_slab_index = 0, .read_slab = ATOMIC_PTR_INIT(g_uart_rx_slabs[1].buf)},
+};
+
+K_MSGQ_DEFINE(g_rx_event_queue_0, sizeof(struct uart_event_rx), UART_EVENT_QUEUE_LEN, 1);
+K_MSGQ_DEFINE(g_rx_event_queue_1, sizeof(struct uart_event_rx), UART_EVENT_QUEUE_LEN, 1);
+static struct k_msgq *const g_rx_event_queue[NUM_PIPES] = {
+    &g_rx_event_queue_0,
+    &g_rx_event_queue_1,
+};
+
+static struct k_poll_event g_poll_events[NUM_PIPES] = {
+    K_POLL_EVENT_STATIC_INITIALIZER(K_POLL_TYPE_MSGQ_DATA_AVAILABLE, K_POLL_MODE_NOTIFY_ONLY,
+                                    &g_rx_event_queue_0, 0),
+    K_POLL_EVENT_STATIC_INITIALIZER(K_POLL_TYPE_MSGQ_DATA_AVAILABLE, K_POLL_MODE_NOTIFY_ONLY,
+                                    &g_rx_event_queue_1, 0),
+};
+
 static void UartCallback(const struct device *uart, struct uart_event *event, void *user_data) {
+  // TODO: Get rid of hardcoded pipe.
+  const uint32_t pipe = 0;
+  UartRxSlabs *const rx_slabs = &g_uart_rx_slabs[pipe];
+  struct k_msgq *const event_queue = g_rx_event_queue[pipe];
+
   switch (event->type) {
     case UART_RX_BUF_REQUEST:
-      g_uart_rx_slab.write_index = (g_uart_rx_slab.write_index + 1) % NUM_UART_RX_BUF;
-
-      uint8_t *const next_buf = g_uart_rx_slab.buf[g_uart_rx_slab.write_index];
-
-      // Buffer is being updated to what was last read in main thread indicating likely overrun.
-      if (next_buf == g_uart_rx_slab.read_buf) {
-        g_stats.errors.rx_overflow++;
-        k_msgq_purge(&g_rx_packet_queue);
+      if (++rx_slabs->write_slab_index >= UART_NUM_SLABS) {
+        rx_slabs->write_slab_index = 0;
       }
 
-      if (uart_rx_buf_rsp(uart, next_buf, TRANSMIT_PACKET_LEN) < 0) {
-        g_stats.errors.other++;
+      uint8_t *const next_buf = rx_slabs->buf[rx_slabs->write_slab_index];
+
+      // Next buffer is being updated to what was last read in main thread indicating an overrun is
+      // possible. Purge event queue so that main thread drops bytes and catches up.
+      if (next_buf == atomic_ptr_get(&rx_slabs->read_slab)) {
+        atomic_inc(&g_stats.errors.rx_overflow);
+        k_msgq_purge(event_queue);
       }
 
-      break;
-    case UART_RX_RDY: {
-      // This copies uart_event_rx twice (once into UartRxPacket and again into the queue), but as
-      // it is only 3 words this is deemed faster than implementing an allocated approach (FIFO).
-      const UartRxPacket rx_packet = {.event = event->data.rx, .pipe = 0};
-      if (k_msgq_put(&g_rx_packet_queue, &rx_packet, K_NO_WAIT) < 0) {
-        g_stats.errors.pkt_overflow++;
-        k_msgq_purge(&g_rx_packet_queue);
+      if (uart_rx_buf_rsp(uart, next_buf, TX_MAX_PKT_LEN) < 0) {
+        atomic_inc(&g_stats.errors.other);
       }
       break;
-    }
+    case UART_RX_RDY:
+      if (k_msgq_put(event_queue, &event->data.rx, K_NO_WAIT) < 0) {
+        atomic_inc(&g_stats.errors.pkt_overflow);
+        k_msgq_purge(event_queue);
+      }
+      break;
     case UART_TX_DONE:
     case UART_TX_ABORTED:
     case UART_RX_BUF_RELEASED:
@@ -88,7 +110,7 @@ static void EsbHandler(const struct esb_evt *event) {
 
 static int SetupUart(const struct device *uart) {
   const struct uart_config uart_config = {
-      .baudrate = BAUD,
+      .baudrate = UART_BAUD,
       .parity = UART_CFG_PARITY_NONE,
       .stop_bits = UART_CFG_STOP_BITS_1,
       .data_bits = UART_CFG_DATA_BITS_8,
@@ -105,7 +127,11 @@ static int SetupUart(const struct device *uart) {
     return -1;
   }
 
-  if (uart_rx_enable(uart, g_uart_rx_slab.buf[0], TRANSMIT_PACKET_LEN, RX_TIMEOUT_US) < 0) {
+  // TODO: Get rid of hardcoded pipe.
+  const uint32_t pipe = 0;
+  UartRxSlabs *const rx_slabs = &g_uart_rx_slabs[pipe];
+
+  if (uart_rx_enable(uart, rx_slabs->buf[0], TX_MAX_PKT_LEN, UART_TIMEOUT_US) < 0) {
     LOG_ERR("Could not enable uart rx.");
     return -1;
   }
@@ -148,10 +174,10 @@ static int SetupEsbTx(void) {
       .bitrate = ESB_BITRATE_1MBPS,
       .crc = ESB_CRC_16BIT,
       .tx_output_power = ESB_TX_POWER_4DBM,
-      .retransmit_delay = RETRANSMIT_DELAY_US,
-      .retransmit_count = RETRANSMIT_COUNT,
+      .retransmit_delay = TX_RETRANSMIT_DELAY_US,
+      .retransmit_count = TX_RETRANSMIT_COUNT,
       .tx_mode = ESB_TXMODE_AUTO,
-      .payload_length = TRANSMIT_PACKET_LEN,
+      .payload_length = TX_MAX_PKT_LEN,
       .selective_auto_ack = true,
   };
 
@@ -193,38 +219,60 @@ void main(void) {
 
   const struct device *const uart = DEVICE_DT_GET(DT_NODELABEL(UART));
   if (!device_is_ready(uart)) {
-    ++g_stats.errors.other;
+    atomic_inc(&g_stats.errors.other);
     LOG_ERR("Could not get uart.");
     return;
   }
 
   if (SetupUart(uart) < 0) {
-    ++g_stats.errors.other;
+    atomic_inc(&g_stats.errors.other);
     return;
   };
 
   if (SetupEsbTx() < 0) {
-    ++g_stats.errors.other;
+    atomic_inc(&g_stats.errors.other);
     return;
   }
 
   struct esb_payload tx_payload = {
       .noack = false,
   };
+  (void)tx_payload;
+
+  // TODO: Get rid of hardcoded pipe.
+  const uint32_t pipe = 0;
+  struct k_msgq *const event_queue = g_rx_event_queue[pipe];
 
   while (true) {
-    UartRxPacket rx_packet;
-    if (k_msgq_get(&g_rx_packet_queue, &rx_packet, K_FOREVER) < 0) {
-      continue;
+    if (k_poll(g_poll_events, ARRAY_SIZE(g_poll_events), K_FOREVER) < 0) {
+      atomic_inc(&g_stats.errors.other);
+      goto reset_poll_events;
     }
 
-    g_uart_rx_slab.read_buf = rx_packet.event.buf;
-    g_stats.rx_bytes += rx_packet.event.len;
+    if (g_poll_events[0].state != K_POLL_STATE_MSGQ_DATA_AVAILABLE) {
+      atomic_inc(&g_stats.errors.other);
+      goto reset_poll_events;
+    }
+
+    struct uart_event_rx event;
+    if (k_msgq_get(event_queue, &event, K_NO_WAIT) < 0) {
+      atomic_inc(&g_stats.errors.other);
+      goto reset_poll_events;
+    }
+
+    UartRxSlabs *const rx_slabs = &g_uart_rx_slabs[pipe];
+    atomic_ptr_set(&rx_slabs->read_slab, event.buf);
+    atomic_add(&g_stats.rx_bytes, event.len);
 
     // Copy data into esb payload.
-    const uint8_t * const rx_data = rx_packet.event.buf + rx_packet.event.offset;
-    tx_payload.length = rx_packet.event.len;
-    tx_payload.pipe = rx_packet.pipe;
-    memcpy(tx_payload.data, rx_data, rx_packet.event.len);
+    // const uint8_t *const rx_data = rx_packet.event.buf + rx_packet.event.offset;
+    // tx_payload.length = rx_packet.event.len;
+    // tx_payload.pipe = rx_packet.pipe;
+    // memcpy(tx_payload.data, rx_data, rx_packet.event.len);
+
+reset_poll_events:
+    for (uint32_t i = 0; i < ARRAY_SIZE(g_poll_events); ++i) {
+      g_poll_events[i].state = K_POLL_STATE_NOT_READY;
+    }
   }
 }
